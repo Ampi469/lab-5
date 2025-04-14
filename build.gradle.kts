befbc716f18d73efbddc0307e971f2e786f1700f
unknown <bibikovvlad5@gmail.com>
2025-04-14 20:53:47 +0300

    changes build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,26 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "itmo.programming"
 version = "1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 repositories {
     mavenCentral()
 }
 
+application {
+    mainClass.set("itmo.programming.Main")
+}
+
 dependencies {
+    implementation("com.google.code.gson:gson:2.10.1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
