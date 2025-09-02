86a831cb3cfc3b2a770032e8ff5f4144122e2b32
Vladislav <vladislav@MacBook-Air-Vladislav.local>
2025-09-02 08:16:46 +0300

    removal of 12121112

--- 121212/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"itmo.programming"},{"l":"itmo.programming.command"},{"l":"itmo.programming.date"},{"l":"itmo.programming.manager"},{"l":"itmo.programming.model"},{"l":"itmo.programming.model.create"}];updateSearchResults();
